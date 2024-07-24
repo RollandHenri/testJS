707bdc45732d4c056eabfa3060e4e98e6838960e
Rolland Henri <rolland.h.dev@gmail.com>
2024-07-24 22:27:07 +0200

    javascript cours 1

--- script.js
+++ script.js
@@ -5,14 +5,16 @@ const btn1 = document.querySelector("#btn-1");
 const btn2 = document.getElementById("btn-2");
 const answer = document.querySelector("p");
 
-console.log(answer);
-
 btn2.addEventListener("click", () => {
   answer.classList.add("badAnswerJS");
 });
 btn1.addEventListener("click", () => {
   answer.classList.add("greatAnswerJS");
 });
-console.log();
 
 /*--------- Mouse Event -------- */
+
+const mousemove = document.querySelector(".mousemove");
+
+window.addEventListener("mousemove", () => {});
+console.log(mousemove);
