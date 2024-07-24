19c50910b9d7a595c14cf3d87e61f40393835c0d
Rolland Henri <rolland.h.dev@gmail.com>
2024-07-24 17:30:25 +0200

    cour1 javascript

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-let answer = document.querySelector("p");
+/* ------------ Cilck Event ----------*/
+
+const container = document.querySelector(".clickEvent");
+const btn1 = document.querySelector("#btn-1");
+const btn2 = document.getElementById("btn-2");
+const answer = document.querySelector("p");
+
+console.log(answer);
+
+btn2.addEventListener("click", () => {
+  answer.classList.add("badAnswerJS");
+});
+btn1.addEventListener("click", () => {
+  answer.classList.add("greatAnswerJS");
+});
+console.log();
+
+/*--------- Mouse Event -------- */
